dfb954f5290fe03df65689aa0c1286ff15d6fc3b
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-07-26 11:04:58 +0200

    Rename original colour test script and add another.

---
+++ colour-test-1.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+echo "Foreground"
+echo -e "\e[0;30mXXXXXXXXXXX\e[0m Black"
+echo -e "\e[1;30mXXXXXXXXXXX\e[0m Black"
+echo -e "\e[2;30mXXXXXXXXXXX\e[0m Black"
+
+echo -e "\e[31mXXXXXXXXXXX\e[0m Red"
+echo -e "\e[31mXXXXXXXXXXX\e[0m Red"
+echo -e "\e[31mXXXXXXXXXXX\e[0m Red"
+
+echo -e "\e[32mXXXXXXXXXXX\e[0m Green"
+echo -e "\e[32mXXXXXXXXXXX\e[0m Green"
+echo -e "\e[32mXXXXXXXXXXX\e[0m Green"
+
+echo -e "\e[33mXXXXXXXXXXX\e[0m Yellow"
+echo -e "\e[33mXXXXXXXXXXX\e[0m Yellow"
+echo -e "\e[33mXXXXXXXXXXX\e[0m Yellow"
+
+echo -e "\e[34mXXXXXXXXXXX\e[0m Blue"
+echo -e "\e[34mXXXXXXXXXXX\e[0m Blue"
+echo -e "\e[34mXXXXXXXXXXX\e[0m Blue"
+
+echo -e "\e[35mXXXXXXXXXXX\e[0m Magenta"
+echo -e "\e[35mXXXXXXXXXXX\e[0m Magenta"
+echo -e "\e[35mXXXXXXXXXXX\e[0m Magenta"
+
+echo -e "\e[36mXXXXXXXXXXX\e[0m Cyan"
+echo -e "\e[36mXXXXXXXXXXX\e[0m Cyan"
+echo -e "\e[36mXXXXXXXXXXX\e[0m Cyan"
+
+echo -e "\e[37mXXXXXXXXXXX\e[0m White"
+echo -e "\e[37mXXXXXXXXXXX\e[0m White"
+echo -e "\e[37mXXXXXXXXXXX\e[0m White"
+
+echo -e "\e[90mXXXXXXXXXXX\e[0m Bright Black"
+echo -e "\e[90mXXXXXXXXXXX\e[0m Bright Black"
+echo -e "\e[90mXXXXXXXXXXX\e[0m Bright Black"
+
+echo -e "\e[91mXXXXXXXXXXX\e[0m Bright Red"
+echo -e "\e[91mXXXXXXXXXXX\e[0m Bright Red"
+echo -e "\e[91mXXXXXXXXXXX\e[0m Bright Red"
+
+echo -e "\e[92mXXXXXXXXXXX\e[0m Bright Green"
+echo -e "\e[92mXXXXXXXXXXX\e[0m Bright Green"
+echo -e "\e[92mXXXXXXXXXXX\e[0m Bright Green"
+
+echo -e "\e[93mXXXXXXXXXXX\e[0m Bright Yellow"
+echo -e "\e[93mXXXXXXXXXXX\e[0m Bright Yellow"
+echo -e "\e[93mXXXXXXXXXXX\e[0m Bright Yellow"
+
+echo -e "\e[94mXXXXXXXXXXX\e[0m Bright Blue"
+echo -e "\e[94mXXXXXXXXXXX\e[0m Bright Blue"
+echo -e "\e[94mXXXXXXXXXXX\e[0m Bright Blue"
+
+echo -e "\e[95mXXXXXXXXXXX\e[0m Bright Magenta"
+echo -e "\e[95mXXXXXXXXXXX\e[0m Bright Magenta"
+echo -e "\e[95mXXXXXXXXXXX\e[0m Bright Magenta"
+
+echo -e "\e[96mXXXXXXXXXXX\e[0m Bright Cyan"
+echo -e "\e[96mXXXXXXXXXXX\e[0m Bright Cyan"
+echo -e "\e[96mXXXXXXXXXXX\e[0m Bright Cyan"
+
+echo -e "\e[97mXXXXXXXXXXX\e[0m Bright White"
+echo
+echo "Background"
+echo -e "\e[40m           \e[0m Black"
+echo -e "\e[1;40m           \e[0m Black"
+echo -e "\e[2;40m           \e[0m Black"
+
+echo -e "\e[41m           \e[0m Red"
+echo -e "\e[42m           \e[0m Green"
+echo -e "\e[43m           \e[0m Yellow"
+echo -e "\e[44m           \e[0m Blue"
+echo -e "\e[45m           \e[0m Magenta"
+echo -e "\e[46m           \e[0m Cyan"
+echo -e "\e[47m           \e[0m White"
+echo -e "\e[100m           \e[0m Bright Black"
+echo -e "\e[101m           \e[0m Bright Red"
+echo -e "\e[102m           \e[0m Bright Green"
+echo -e "\e[103m           \e[0m Bright Yellow"
+echo -e "\e[104m           \e[0m Bright Blue"
+echo -e "\e[105m           \e[0m Bright Magenta"
+echo -e "\e[106m           \e[0m Bright Cyan"
+echo -e "\e[107m           \e[0m Bright White"
+
+Result of echo -e "\e[0mHello\e[0m" "\e[1mHello\e[0m" "\e[2mHello\e[0m"; with the default Linux.colorscheme:
